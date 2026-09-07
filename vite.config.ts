21180a7a3526fb4b49dece9916ab6d381562b504
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-09-06 22:02:11 -0700

    Remove former project name from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,9 @@ const cloudflareDatabaseId =
   process.env.CLOUDFLARE_D1_DATABASE_ID ??
   SITE_CREATOR_PLACEHOLDER_DATABASE_ID;
 const cloudflareDatabaseName =
-  process.env.CLOUDFLARE_D1_DATABASE_NAME ?? "my-village-auntie-cia";
+  process.env.CLOUDFLARE_D1_DATABASE_NAME ?? "cia-faq";
 const cloudflareBucketName =
-  process.env.CLOUDFLARE_R2_BUCKET_NAME ?? "my-village-auntie-cia-media";
+  process.env.CLOUDFLARE_R2_BUCKET_NAME ?? "cia-faq-media";
 
 const { d1, r2 } = hostingConfig;
 
